e6009ba32f2e43e96ffc3a13841057ddf85bb856
theoronna88 <theo@ronna.com.br>
2025-11-20 18:53:46 -0300

    chore: initial project setup with dependencies

---
+++ src/main/java/br/com/ronna/financerta/FinancertaApplication.java
@@ -0,0 +1,13 @@
+package br.com.ronna.financerta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancertaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancertaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ronna/financerta/FinancertaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ronna.financerta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancertaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
