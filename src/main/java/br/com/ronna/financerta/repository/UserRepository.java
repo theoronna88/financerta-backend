298c0ed3f3059ae5bfe9d6c80616597ef767d0b8
theoronna88 <theo@ronna.com.br>
2025-11-21 23:00:24 -0300

    feat: add phone verification already exists in db

--- src/main/java/br/com/ronna/financerta/repository/UserRepository.java
+++ src/main/java/br/com/ronna/financerta/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package br.com.ronna.financerta.repository;
 
 import br.com.ronna.financerta.model.User;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +13,6 @@ import java.util.UUID;
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
+
+    boolean existsByPhone(@NotBlank(message = "Telefone é obrigatório") @Size(min = 10, max = 15, message = "Telefone deve ter entre 10 e 15 caracteres") String phone);
 }
