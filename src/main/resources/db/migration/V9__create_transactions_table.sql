550be847c2ad4bd01b05ae0a70d3e712bbc115d4
theoronna88 <theo@ronna.com.br>
2025-11-22 12:04:55 -0300

    chore: add migration to add transactions table

---
+++ src/main/resources/db/migration/V9__create_transactions_table.sql
@@ -0,0 +1,18 @@
+CREATE TABLE transactions (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    wallet_id UUID NOT NULL REFERENCES wallets(id) ON DELETE CASCADE,
+    credit_card_id UUID NULLABLE REFERENCES credit_cards(id) ON DELETE CASCADE,
+    category_id UUID NOT NULL REFERENCES transaction_categories(id) ON DELETE CASCADE,
+    type VARCHAR(50) NOT NULL,
+    amount DECIMAL(19,2) NOT NULL,
+    transaction_date TIMESTAMP NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    payment_method VARCHAR(50) NOT NULL,
+    purchase_group_id UUID,
+    installment_number INTEGER,
+    total_installments INTEGER,
+    credit_card_statement_id UUID NULLABLE REFERENCES credit_card_statements(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
