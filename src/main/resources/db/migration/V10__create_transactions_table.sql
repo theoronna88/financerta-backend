a72dd33a6cc5a58fb5b8287e976253c727c40c0d
theoronna88 <theo@ronna.com.br>
2025-11-23 11:40:06 -0300

    feat: add migration to stataments and transactions tabls

--- src/main/resources/db/migration/V10__create_transactions_table.sql
+++ src/main/resources/db/migration/V10__create_transactions_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE transactions (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     wallet_id UUID NOT NULL REFERENCES wallets(id) ON DELETE CASCADE,
-    credit_card_id UUID NULLABLE REFERENCES credit_cards(id) ON DELETE CASCADE,
+    credit_card_id UUID REFERENCES credit_cards(id) ON DELETE CASCADE,
     category_id UUID NOT NULL REFERENCES transaction_categories(id) ON DELETE CASCADE,
     type VARCHAR(50) NOT NULL,
     amount DECIMAL(19,2) NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE transactions (
     purchase_group_id UUID,
     installment_number INTEGER,
     total_installments INTEGER,
-    credit_card_statement_id UUID NULLABLE REFERENCES credit_card_statements(id),
+    credit_card_statement_id UUID REFERENCES credit_card_statements(id),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
